310c3b18ac2b37e072e34609783c41a3fcd90f30
Greg-tts <gtesto@techtalentsouth.com>
2019-10-16 15:48:11 -0700

    added nav bar styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link as RouterLink } from 'react-router-dom';
 import ShowBlogEntries from './components/ShowBlogEntries';
 import ShowBlogEntry from './components/ShowBlogEntry';
 import BlogEntryForm from './components/BlogEntryForm';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import Fab from '@material-ui/core/Fab';
+import Grid from '@material-ui/core/Grid';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import AddIcon from '@material-ui/icons/Add';
+
+const LinkForward = React.forwardRef((props, ref) => <RouterLink innerRef={ref} {...props} />);
+
 
 class App extends React.Component{
   constructor(){
@@ -23,18 +36,34 @@ class App extends React.Component{
     this.fetchEntries();
   }
   render(){
+
+
     return (
       <Router>
+
         <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/create">Create</Link>
-            </li>
-          </ul>
+          <AppBar position="static">
+            <Toolbar>
+              <Grid
+                justify="space-between" // Add it here :)
+                container 
+                spacing={24}
+              >
+                <Grid item>
+                  <Button color="inherit" component={LinkForward} to="/">
+                    Tech Blog
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Fab size="small" color="inherit" component={LinkForward} to="/create">
+                    <AddIcon color="primary"/>
+                  </Fab>
+                </Grid>
+              </Grid>
+            </Toolbar>
+          </AppBar>
         </nav>
+
         <div id="content_body">
           <Switch>
             <Route path="/entry/:id" render={(props)=>(
