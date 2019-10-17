3f47d5d6886e57c5f5a2bc0ff9967d6d1b364463
Greg-tts <gtesto@techtalentsouth.com>
2019-10-17 14:07:41 -0700

    Added styling to form and posts page

--- src/components/BlogEntryForm.js
+++ src/components/BlogEntryForm.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Icon from '@material-ui/core/Icon';
+import Grid from '@material-ui/core/Grid';
 
 const BlogEntryForm=(props)=>{
     const [id, setId] = React.useState(0);
@@ -67,18 +71,77 @@ const BlogEntryForm=(props)=>{
     }, [])
 
     let buttonAction;
+
+      
+
     if(id){
-        buttonAction = <button onClick={handleUpdateClick}>Update</button>
+        buttonAction = (
+            <Button
+                variant="contained"
+                color="primary"
+                onClick={handleUpdateClick}
+                endIcon={<Icon>send</Icon>}
+              >
+                Update
+              </Button>
+        )
     } else {
-        buttonAction = <button onClick={handleCreateClick}>Create</button>
+
+        buttonAction = (
+            <Button
+                variant="contained"
+                color="primary"
+                onClick={handleCreateClick}
+                endIcon={<Icon>send</Icon>}
+              >
+                Create
+              </Button>
+        )
     }
     return(
         <div>
-            <h1>Blog Entry Form</h1>
-            <input type="text" value={title} onChange={onTitleInput} placeholder="Title of entry"/>
-            <input type="text" value={author} onChange={onAuthorInput} placeholder="Author of entry"/>
-            <input type="text" value={content} onChange={onContentInput} placeholder="Content of entry"/>
-            {buttonAction}
+
+        <Grid
+              container
+              spacing={0}
+              direction="column"
+              alignItems="center"
+              justify="center"
+            >
+              <Grid item xs={10}>
+                <h1>Blog Post</h1>
+                <TextField
+                    id="outlined-name"
+                    label="Title"
+                    value={title}
+                    onChange={onTitleInput}
+                    margin="normal"
+                    variant="outlined"
+                    fullWidth
+                  />
+                <TextField
+                    id="outlined-name"
+                    label="Author"
+                    value={author}
+                    onChange={onAuthorInput}
+                    margin="normal"
+                    variant="outlined"
+                    fullWidth
+                  />
+                <TextField
+                    id="outlined-name"
+                    label="Content"
+                    multiline
+                    rows="4"
+                    value={content}
+                    onChange={onContentInput}
+                    margin="normal"
+                    variant="outlined"
+                    fullWidth
+                  />
+                {buttonAction}
+              </Grid> 
+            </Grid> 
         </div>
     )
 }
--- src/components/ShowBlogEntries.js
+++ src/components/ShowBlogEntries.js
@@ -1,20 +1,59 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link as RouterLink } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import GridList from '@material-ui/core/GridList';
+
+
+const LinkButton = React.forwardRef((props, ref) => <RouterLink innerRef={ref} {...props} />);
+
+const useStyles = makeStyles({
+  card: {
+    minWidth: 200,
+  },
+  title: {
+    fontSize: 14,
+  },
+  headerTitle: {
+    textAlign:'center',
+    margin: 10
+  }
+});
 
 const ShowBlogEntries=(props)=>{
+    const classes = useStyles();
     let blogEntriesElArr = props.blogEntries.map((entry, index)=>{
+        let snippet = entry.content.split("").slice(0, 51).join("") + "...";
         return(
-            <Link to={"/entry/" + entry.id} key={index}>
-                <ul>
-                    <li>{entry.title}</li>
-                </ul>
-            </Link>
+            <Card key={index} className={classes.card}>
+              <CardContent>
+                <Typography className={classes.title} variant="h5" component="h2">
+                  {entry.title}
+                </Typography>
+                <Typography variant="body2" component="p">
+                  {snippet}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button color="inherit" component={LinkButton} to={"/entry/" + entry.id}>
+                    Learn More
+                </Button>
+              </CardActions>
+            </Card>
         )
     })
     return(
         <div>
-            <h1>Show All Entries</h1>
-            {blogEntriesElArr}
+            <Typography className={classes.headerTitle} variant="h2" component="h2">
+              Blog Posts
+            </Typography>
+            <GridList cols={4} spacing={10}>
+                {blogEntriesElArr}
+            </GridList>
         </div>
     )
 }
